cee59158f148f697ecfb4444aac2734cf59435da
Max <max.taylor-hayden@covantagecu.org>
2020-10-29 08:28:18 -0500

    Node.js module

---
+++ Section 18 Node.js/index.js
@@ -0,0 +1,11 @@
+const superheros = require('superheroes');
+
+var mySuperheroName = superheros.random();
+
+console.log(mySuperheroName);
+
+const supervillains = require('supervillains');
+ 
+var supervillain = supervillains.random();
+
+console.log(supervillain);

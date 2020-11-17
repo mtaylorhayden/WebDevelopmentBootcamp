1de02588fd55e935a07af321cedb076d7a5e3c67
Max <max.taylor-hayden@covantagecu.org>
2020-11-16 22:09:44 -0600

    testing some jquery funcs

--- Section 14 JQuery/JQuery/index.js
+++ Section 14 JQuery/JQuery/index.js
@@ -13,3 +13,19 @@
 $("button").click(function () {
     $("h1").slideUp().slideDown().animate({opacity: 0.5})
 });
+
+$("#table tr td").each(function(){
+    console.log(this);
+    if(this.innerText == "Smith"){
+        console.log(this);
+        $(this).hide();
+    }
+})
+
+
+
+// var tbody = $("#table")[0].childNodes[1];
+// console.log(tbody);
+
+// var tr = $(tbody).childern();
+// console.log(tr);

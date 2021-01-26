949cca0f3f19b0a9352379b32a5c5b7f0efc873b
Max <max.taylor-hayden@covantagecu.org>
2021-01-26 12:59:29 -0600

    Completed RESTful API

--- Section-31-Build-Your-Own-RESTful-API/Wiki-API/app.js
+++ Section-31-Build-Your-Own-RESTful-API/Wiki-API/app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
 const mongoose = require('mongoose');
+const {
+    runInContext
+} = require("lodash");
 
 const app = express();
 
@@ -25,20 +28,111 @@ const artilceSchema = new mongoose.Schema({
 
 const Article = new mongoose.model("Article", artilceSchema);
 
-const testing = new Article({
-    title: "Testing",
-    content: "This is a test to see if this works"
-});
+////////////////////////////////////////////// requests targeting ALL articles
 
-app.get("/articles", function (req, res) {
-    Article.find(function (err, foundArticles) {
-        if(!err){
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+app.route("/articles")
+
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        })
+    })
+
+    .post(function (req, res) {
+        console.log(req.body.title);
+        console.log(req.body.content);
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles");
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
+
+////////////////////////////////////////////// requests targeting specific articles
+
+app.route("/articles/:articleTitle")
+
+    .get(function (req, res) {
+        Article.findOne({
+            title: req.params.articleTitle
+        }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No articles matching that title");
+            }
+        });
+    })
+
+    .put(function (req, res) {
+        Article.update({
+                title: req.params.articleTitle
+            }, {
+                title: req.body.title,
+                content: req.body.content
+            }, {
+                overwrite: true
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article");
+                }
+            }
+        );
+    })
+
+    .patch(function (req, res) {
+        Article.update({
+                title: req.params.articleTitle
+            }, {
+                $set: req.body
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    })
+
+    .delete(function (req, res) {
+        console.log("req.params.articleTitle " + req.params.articleTitle);
+        Article.deleteOne({
+                title: req.params.articleTitle
+            },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully deleted the article");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    });
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");

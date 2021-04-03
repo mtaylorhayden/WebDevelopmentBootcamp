d62e9ae160004c240cce0b593ee097bbea6ffd5d
Max Taylor-Hayden <max.taylor-hayden@covantagecu.org>
2021-04-03 18:08:36 -0500

    Completed Keeper App Challenge 1

--- Section-33-React/React/src/index.js
+++ Section-33-React/React/src/index.js
@@ -9,4 +9,13 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in the styles.css file to appy styling.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

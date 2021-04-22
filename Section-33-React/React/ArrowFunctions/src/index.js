714e9812318a6916be9b374a7f4e916de5b8e28b
Max Taylor-Hayden <max.taylor-hayden@covantagecu.org>
2021-04-22 18:12:34 -0500

    Updated keeper app with map function

--- Section-33-React/React/ArrowFunctions/src/index.js
+++ Section-33-React/React/ArrowFunctions/src/index.js
@@ -5,59 +5,59 @@ import Emojipedia from "./emojipedia";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
-function square(x){
-    return x * x;
-}
+// function square(x){
+//     return x * x;
+// }
 
-const newNumbers = numbers.map(square);
+// const newNumbers = numbers.map(square);
 
-console.log(newNumbers);
+// console.log(newNumbers);
 
 
-// or
+// // or
 
-const newNumbers1 = numbers.map(function(x){
-    return x * x;
-});
-
-console.log(newNumbers1);
-
-// arrow function
-
-const newNumbers2 = numbers.map(x =>  x * x );
-
-console.log(newNumbers2);
-
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
+// const newNumbers1 = numbers.map(function(x){
+//     return x * x;
 // });
 
-const newNumbers3 = numbers.filter(num => num < 10);
-console.log(newNumbers3);
+// console.log(newNumbers1);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+// // arrow function
 
-const newNumbers4 = numbers.reduce((accumulator,currentNumber) => accumulator + currentNumber);
-console.log(newNumbers4);
+// const newNumbers2 = numbers.map(x =>  x * x );
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+// console.log(newNumbers2);
 
-const newNumbers5 = numbers.find(num => num > 10);
-console.log(newNumbers5);
+// //////Filter - Create a new array by keeping the items that return true.
+// // const newNumbers = numbers.filter(function(num) {
+// //   return num < 10;
+// // });
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+// const newNumbers3 = numbers.filter(num => num < 10);
+// console.log(newNumbers3);
 
-const newNumbers6 = numbers.findIndex(num => num > 10);
-console.log(newNumbers6);
+// //Reduce - Accumulate a value by doing something to each item in an array.
+// // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+// //     return accumulator + currentNumber;
+// // })
+
+// const newNumbers4 = numbers.reduce((accumulator,currentNumber) => accumulator + currentNumber);
+// console.log(newNumbers4);
+
+// ////Find - find the first item that matches from an array.
+// // const newNumber = numbers.find(function (num) {
+// //   return num > 10;
+// // })
+
+// const newNumbers5 = numbers.find(num => num > 10);
+// console.log(newNumbers5);
+
+// ////FindIndex - find the index of the first item that matches.
+// // const newNumber = numbers.findIndex(function (num) {
+// //   return num > 10;
+// // })
+
+// const newNumbers6 = numbers.findIndex(num => num > 10);
+// console.log(newNumbers6);

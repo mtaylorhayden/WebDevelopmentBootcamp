f32e52e4bf93cdb188c8ac003a502bd6dc43bf26
Max Taylor-Hayden <max.taylor-hayden@covantagecu.org>
2021-04-23 21:01:08 -0500

    Event handling

---
+++ Section-33-React/React/EventHandling/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [headingText, setHeadingText] = useState("Hellooo");
+
+  const [isMousedOver, setMouseOver] = useState(false)
+
+  function handleClick(){
+    setHeadingText("Submitted");
+  }
+
+  function mouseOut(){
+    setMouseOver(false);
+  }
+
+  function mouseOver(){
+    setMouseOver(true);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button style={{backgroundColor: isMousedOver ? "black" : "white"}} onMouseOut={mouseOut} onMouseOver={mouseOver} onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
